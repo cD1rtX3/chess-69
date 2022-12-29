14f0d9d325f8e8af38e600f13cdb33d0042b49ac
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2022-12-29 14:56:30 -0500

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,1 +1,17 @@
+// Instantioating the board
+var b = document.getElementById("board")
+var size = [8, 8]
+b.scale(420/(240*size[0]), 420/(240*size[1]))
 
+// Light and dark squares
+let squares = [[]]
+for (let i = 0; i < size[0]; i++) {
+	for (let j = 0; j < size[1]; j++) {
+		if (((size[0]-i)+(size[1]-j))%2) {
+			b.fillStyle = "#44B" // Dark square
+		} else {
+			b.fillStyle = "#99D" // Light square
+		}
+	}
+	b.fillRect(240*i, 240*j, 240, 240)
+}
