52a9c21d5db3182e1c24d6a0b09e4463bdd0d8bb
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2023-01-01 22:24:39 -0500

    Update chess.js

--- chess.js
+++ chess.js
@@ -8,27 +8,38 @@ b.scale(300/(240*size[0]), 150/(240*size[1]))
 let squares = [[]]
 for (let i = 0; i < size[0]; i++) {
 	for (let j = 0; j < size[1]; j++) {
-		if (((size[0]-i)+(size[1]-j))%2) {
-			b.fillStyle = "#669" // Dark square
-		} else {
-			b.fillStyle = "#77B" // Light square
-		}
+		b.fillStyle = ((i+(size[1]-j))%2) ? "#77B" : "#669" // Chequerboard
 		b.fillRect(240*i, 240*j, 240, 240)
 	}
 }
 
 // Piece class
 class Piece {
-	img; x; y
-	constructor (p, x, y) { // TODO add moveset, captureset
+	img
+	constructor (p) { // TODO add moveset, captureset
 		this.img = new Image(240, 240)
 		this.img.src = "./img/" + p + ".png"
-		this.x = x * 240
-		this.y = y * 240
 	}
 }
 
 // Piece test
-var pp = []
-pp[0] = new Piece("bR", 0, 0)
-b.drawImage(pp[0].img, pp[0].x, pp[0].y)
+var p = []
+for (let i = 0; i < 32; i++) {
+	if (i < 16) {
+		p[i] = (i < 8) ? new Piece("bP") : new Piece("wP") // Pawns
+	} else {
+		p[i] = new Piece(((i < 24) ? "b" : "w") + "RSBQKBSR".split('')[i%8]) // Backranks
+	}
+}
+
+// Attempt at board
+var board = [
+	[p[16], p[17], p[18], p[19], p[20], p[21], p[22], p[23]],
+	[ p[0],  p[1],  p[2],  p[3],  p[4],  p[5],  p[6],  p[7]],
+	[ null,  null,  null,  null,  null,  null,  null,  null],
+	[ null,  null,  null,  null,  null,  null,  null,  null],
+	[ null,  null,  null,  null,  null,  null,  null,  null],
+	[ null,  null,  null,  null,  null,  null,  null,  null],
+	[ p[8],  p[9], p[10], p[11], p[12], p[13], p[14], p[15]],
+	[p[24], p[25], p[26], p[27], p[28], p[29], p[30], p[31]]
+]
